69722169a23cb72f0aeb9de55a4877c53a14e70e
Lourence Linao <lourencelinao13@gmail.com>
2020-09-26 23:03:07 +0800

    heroku config

--- server/index.js
+++ server/index.js
@@ -12,6 +12,15 @@ app.use(cors())
 const todolist = require('./routes/api/todolist')
 app.use('/api/todolist', todolist)
 
+if(process.env.NODE_ENV === 'production'){
+    // static folder
+    app.use(express.static(__dirname + '/public/'))
+
+    // SPA handler
+    app.get(/.*/, (req, res) => {
+        res.sendFile(__dirname + '/public/index.html')
+    })
+}
 
 // port
 const port = process.env.PORT || 3000
